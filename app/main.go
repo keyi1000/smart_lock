2e417db54bbab7f48dd1944e8593ac4881b64572
keyi1000 <keyi0351fps@gmail.com>
2026-01-26 16:34:45 +0900

    argocd動作テスト

--- app/main.go
+++ app/main.go
@@ -63,7 +63,7 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 
 	resp := Response{
 		Status:  "healthy",
-		Message: "Smart Lock api is running",
+		Message: "Smart Lock API is running",
 	}
 
 	json.NewEncoder(w).Encode(resp)

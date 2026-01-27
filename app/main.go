5874f07d3828e1a140cf01bb16e27af1de374052
keyi1000 <keyi0351fps@gmail.com>
2026-01-27 13:33:22 +0900

    webhookを無しにしてテスト

--- app/main.go
+++ app/main.go
@@ -63,7 +63,7 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 
 	resp := Response{
 		Status:  "healthy",
-		Message: "Smart Lock testAPI is running",
+		Message: "Smart Lock nowebhookAPI is running",
 	}
 
 	json.NewEncoder(w).Encode(resp)
